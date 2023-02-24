dad925cbfd882a60f697e359e2a968559ee827b3
yaroslav-android <yaroslav.ovdiienko@uptech.team>
2023-02-24 13:56:17 +0200

    chore: verify CI build pass t-17

--- bridge/test/widget_test.dart
+++ bridge/test/widget_test.dart
@@ -1,1 +1,9 @@
-void main() {}
+import 'package:bridge/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('blank test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+  });
+}
